c81f0d69c683b0d039bb609e54ca7da328d6ca17
David Ruhe <ruhe@lofar.eu>
2021-02-11 08:16:54 +0000

    unneeded flag

--- lpf/surveys/survey.py
+++ lpf/surveys/survey.py
@@ -8,17 +8,20 @@ from lpf.bolts.io import rglob
 from typing import Tuple
 import os
 import logging
+
 logger = logging.getLogger(__name__)
 
 RAISE_NAN_WARN_FRACTION = 1 / 4
 
 
 class Survey:
-    def __init__(self, 
-                 path: str, 
-                 timestamp_start_stop: Tuple[int, int], 
-                 subband_start_stop: Tuple[int, int],
-                 dt: int):
+    def __init__(
+        self,
+        path: str,
+        timestamp_start_stop: Tuple[int, int],
+        subband_start_stop: Tuple[int, int],
+        dt: int,
+    ):
 
         self.survey_length = None
         self.timestamp_start_stop: Tuple[int, int] = timestamp_start_stop
@@ -26,18 +29,26 @@ class Survey:
 
         self.data: pd.DataFrame = self._setup(path)
         len_before = len(self.data)
-        self.data = self.data.drop_duplicates(['timestamp','band'], keep='last')
+        self.data = self.data.drop_duplicates(["timestamp", "band"])
         len_after = len(self.data)
         if (len_before - len_after) > 0:
-            logger.warning("%s timestep-subband duplicates were found and dropped.", len_before - len_after)
-        self.data = self.data.set_index('timestamp')
+            logger.warning(
+                "%s timestep-subband duplicates were found and dropped.",
+                len_before - len_after,
+            )
+        self.data = self.data.set_index("timestamp")
 
         # This resamples the data onto the specified delta T grid. Filling NaN when images are missing.
-        self.indexed_data = self.data.pivot(columns='band').resample(f'{dt}S').first().stack(dropna=False)
+        self.indexed_data = (
+            self.data.pivot(columns="band")
+            .resample(f"{dt}S")
+            .first()
+            .stack(dropna=False)
+        )
 
         self.num_bands = len(self.indexed_data.index.unique(level=-1))
 
-        assert (self.indexed_data.groupby('timestamp').size() == self.num_bands).all()
+        assert (self.indexed_data.groupby("timestamp").size() == self.num_bands).all()
         self.indexed_data = self.indexed_data.sort_index(ascending=[True, False])
         self.time_index = self.indexed_data.index.unique(level=0)
 
@@ -63,7 +74,7 @@ class Survey:
             {
                 "file": files,
                 "timestamp_str": timestamps,
-                "timestamp": pd.to_datetime(timestamps, errors='coerce'),  # type: ignore
+                "timestamp": pd.to_datetime(timestamps, errors="coerce"),  # type: ignore
                 "band": bands,
             }
         ).dropna()
@@ -84,5 +95,7 @@ class Survey:
         timestep = self.indexed_data.loc[self.time_index[index]]
         num_nan = timestep.isna().any(axis=1).sum()
         if num_nan > len(timestep) * RAISE_NAN_WARN_FRACTION:
-            logger.warning("Encountered %s sub-bands without an image at index %s.", num_nan, index)
+            logger.warning(
+                "Encountered %s sub-bands without an image at index %s.", num_nan, index
+            )
         return timestep
